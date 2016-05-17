4568834b087d1867c27246ef73c24d945f5a685b
liuchundian <liuchundian@bytedance.com>
2016-05-18 00:48:25 +0800

    deploy: download plugins

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cd $HOME
 
 all_rc=(.zshrc .vimrc)
 
-for rc in $all_rc;
+for rc in {$all_rc[@]};
 do
   if [[ -f $rc ]]; then
     cp -v $rc $rc.bak
@@ -18,9 +18,20 @@ do
   fi
 done
 
-
-plugin_dir=$HOME/dotfiles/.oh-my-zsh/custom/plugins
 github=https://github.com
+# download .oh-my-zsh
+[[ ! -d .oh-my-zsh ]] && git clone $github/robbyrussell/oh-my-zsh.git .oh-my-zsh
+
+# download plugins for tmux
+tmux_plugins=(tpm tmux-copycat tmux-resurrect tmux-yank)
+for plugin in ${tmux_plugins[@]};
+do
+  dest=~/dotfiles/.tmux/plugins/$plugin
+  [[ -d $dest ]] && rm -dfr $dest && git clone $github/tmux-plugins/$plugin $dest
+done
+
+# download plugins for zsh
+plugin_dir=$HOME/dotfiles/.oh-my-zsh/custom/plugins
 [ ! -d $plugin_dir/zsh-autosuggestions ] \
   && git clone $github/zsh-users/zsh-autosuggestions.git $plugin_dir/zsh-autosuggestions
 [ ! -d $plugin_dir/zsh-syntax-highlighting ] \
